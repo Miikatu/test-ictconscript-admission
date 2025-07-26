3b4366f1b9e9883317591e6f49c233240424b51d
miikatu <miikatti@gmail.com>
2025-07-26 20:05:08 +0300

    Fix: Broken Build Paths

--- 01a-swdev-frontend/next.config.ts
+++ 01a-swdev-frontend/next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'test-ictconscript-admission';
+const repoName = '01a-swdev-frontend';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: isProd ? `/${repoName}/out` : '',
+  assetPrefix: isProd ? `/${repoName}/out` : '',
 };
 
 export default nextConfig;
