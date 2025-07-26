d8d6a73c49f9f6002bf8f91b2f458fb8c8fa4cda
miikatu <miikatti@gmail.com>
2025-07-26 20:18:58 +0300

    Fix: gh-pages wrong paths

--- 01a-swdev-frontend/next.config.ts
+++ 01a-swdev-frontend/next.config.ts
@@ -5,8 +5,8 @@ const repoName = '01a-swdev-frontend';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? `/${repoName}/out` : '',
-  assetPrefix: isProd ? `/${repoName}/out` : '',
+  basePath: '',
+  assetPrefix: '',
 };
 
 export default nextConfig;
